bd9679b5daca9e59faf34b3f1a3ca1a7e9c538b9
SIEA <69807609+sieadev@users.noreply.github.com>
2023-09-11 00:01:33 +0200

    Add files via upload

---
+++ src/main/java/dev/siea/almostuselesscommands/AlmostUselessCommands.java
@@ -0,0 +1,17 @@
+package dev.siea.almostuselesscommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AlmostUselessCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
